9f70d2c9d6a77c16d433ac7e849d28c7a55f795b
SpidySamurai <javierchiortiz@gmail.com>
2021-07-08 20:24:17 -0500

    Adding Square calculate code

---
+++ figuras.js
@@ -0,0 +1,9 @@
+//Square code ...
+const SquareSide = 5;
+console.log("Square sides are: " + SquareSide + "cm");
+
+const SquarePerimeter = SquareSide * 4;
+console.log("Square perimeter is: " + SquarePerimeter + "cm");
+
+const SquareArea = SquareSide * SquareSide;
+console.log("Square area is: " + SquareArea + "cm^2");
