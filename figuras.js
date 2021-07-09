b45561c69a9d3768456d475486d2d07cbddb9094
SpidySamurai <javierchiortiz@gmail.com>
2021-07-08 21:55:55 -0500

    Encapsulating shapes functions

--- figuras.js
+++ figuras.js
@@ -1,54 +1,75 @@
 //Square code ...
 console.group("Square calculations");
-const SquareSide = 5;
-console.log("Square sides are: " + SquareSide + "cm");
-
-const SquarePerimeter = SquareSide * 4;
-console.log("Square perimeter is: " + SquarePerimeter + "cm");
-
-const SquareArea = SquareSide * SquareSide;
-console.log("Square area is: " + SquareArea + "cm^2");
+// const squareSide = 5;
+// console.log("square sides are: " + squareSide + "cm");
+function calculateSquarePerimeter(squareSide) {
+  return squareSide * 4;
+}
+// const SquarePerimeter = SquareSide * 4;
+// console.log("Square perimeter is: " + SquarePerimeter + "cm");
+function calculateSquareArea(squareSide) {
+  return squareSide ** 2;
+}
+// const SquareArea = SquareSide * SquareSide;
+// console.log("Square area is: " + SquareArea + "cm^2");
 console.groupEnd;
 
 //Triangle code...
 console.group("Triangule calculations");
-const TriangleSideA = 5;
-const TriangleSideB = 5;
-const TriangleBase = 10;
-const TriangleHeight = 5.5;
-console.log(
-  "Triangle side A measures: " +
-    TriangleSideA +
-    "\nTriangle side B measures: " +
-    TriangleSideB +
-    "\nTriangle base measures: " +
-    TriangleBase +
-    "\nTriangle height measures: " +
-    TriangleHeight
-);
-
-const TrianglePerimeter = TriangleSideA + TriangleSideB + TriangleBase;
-console.log("Triangle perimeter is: " + TrianglePerimeter + "cm");
-
-const TriangleArea = (TriangleBase * TriangleHeight) / 2;
-console.log("Triangle area is: " + TriangleArea + "cm^2");
+// const triangleSideA = 5;
+// const triangleSideB = 5;
+// const triangleBase = 10;
+// const triangleHeight = 5.5;
+// console.log(
+//   "Triangle side A measures: " +
+//     TriangleSideA +
+//     "\nTriangle side B measures: " +
+//     TriangleSideB +
+//     "\nTriangle base measures: " +
+//     TriangleBase +
+//     "\nTriangle height measures: " +
+//     TriangleHeight
+// );
+function calculateTrianglePerimeter(
+  triangleSideA,
+  triangleSideB,
+  triangleBase
+) {
+  return triangleSideA + triangleSideB + triangleBase;
+}
+// const trianglePerimeter = triangleSideA + triangleSideB + triangleBase;
+// console.log("Triangle perimeter is: " + TrianglePerimeter + "cm");
+function calculateTriangleArea(triangleBase, triangleHeight) {
+  return (triangleBase * triangleHeight) / 2;
+}
+// const TriangleArea = (TriangleBase * TriangleHeight) / 2;
+// console.log("Triangle area is: " + TriangleArea + "cm^2");
 console.groupEnd;
 
 //Circle code ...
 console.group("Circle calculations");
-const CircleRadius = 5;
-const CircleDiameter = CircleRadius * 2;
-const PI = Math.PI;
-console.log(
-  "Circle radius measures: " +
-    CircleRadius +
-    "\nCircle diameter measures: " +
-    CircleDiameter +
-    "\nPI: " +
-    PI
-);
-const CircleCircunference = CircleDiameter * PI;
-console.log("Circle circuference is: " + CircleCircunference + "cm");
-const CircleArea = CircleRadius * CircleRadius * PI;
-console.log("Circle area is: " + CircleArea + "cm");
+// const CircleRadius = 5;
+// const CircleDiameter = CircleRadius * 2;
+// const PI = Math.PI;
+// console.log(
+//   "Circle radius measures: " +
+//     CircleRadius +
+//     "\nCircle diameter measures: " +
+//     CircleDiameter +
+//     "\nPI: " +
+//     PI
+// );
+function calculateCircleDiameter(circleRadius) {
+  return circleRadius * 2;
+}
+function calculateCircleCircunference(circleRadius) {
+  return calculateCircleDiameter(circleRadius) * Math.PI;
+}
+// const circleCircunference = circleDiameter * PI;
+// console.log("Circle circuference is: " + circleCircunference + "cm");
+function calculateCircleArea(circleRadius) {
+  return circleRadius ** 2 * Math.PI;
+}
+// const circleArea = circleRadius * circleRadius * PI;
+// console.log("circle area is: " + circleArea + "cm");
 console.groupEnd;
